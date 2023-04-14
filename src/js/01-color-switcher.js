8fb198ad38640c7bf6842da35d75d4ca18f4579c
Vital3414 <qwer1234432148@gmail.com>
2023-04-14 10:34:25 +0300

    hw 9 task 1

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,37 @@
+let isActive = false;
+let timeoutId;
+
+const refs = {
+  start: document.querySelector('[data-start]'),
+  stop: document.querySelector('[data-stop]'),
+};
+
+refs.stop.disabled = true;
+
+refs.start.addEventListener('click', onStartClick);
+refs.stop.addEventListener('click', onStopClick);
+
+function onStartClick(e) {
+  if (timeoutId) {
+    return;
+  }
+
+  timeoutId = setInterval(backgroundChanger, 1000);
+  refs.start.disabled = true;
+  refs.stop.disabled = false;
+}
+
+function onStopClick(e) {
+  clearInterval(timeoutId);
+  timeoutId = 0;
+  refs.start.disabled = false;
+  refs.stop.disabled = true;
+}
+
+function backgroundChanger() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
